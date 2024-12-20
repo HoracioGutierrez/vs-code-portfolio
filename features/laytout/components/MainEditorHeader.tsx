536d01c60ca375f77429136f13603390a33520f6
Horacio Gutierrez <horacio.estevez@gmail.com>
2024-12-19 23:23:29 -0300

    feat(layout): enhance MainEditorHeader with contact info
    - Added ContainerWithBorder for contact display on larger screens.

--- features/laytout/components/MainEditorHeader.tsx
+++ features/laytout/components/MainEditorHeader.tsx
@@ -3,6 +3,7 @@ import { Fira_Code } from "next/font/google"
 import MainEditorNavigation from "./MainEditorNavigation"
 import { motion } from "motion/react"
 import { Menu } from "lucide-react"
+import ContainerWithBorder from "./ContainerWithBorder"
 
 const firaCode = Fira_Code({ weight: "400", subsets: ["latin"] })
 
@@ -19,7 +20,10 @@ function MainEditorHeader({ ref }: MainEditorHeaderProps) {
     >
       <h1 className="text-muted-foreground pl-4 lg:pl-5.5 py-4 transition-all border-r border-muted-foreground pr-39">horacio-gutierrez</h1>
       <MainEditorNavigation />
-      <Menu className="self-center ml-auto mr-4 text-muted-foreground -motion-translate-x-in-100"/>
+      <Menu className="self-center ml-auto mr-4 text-muted-foreground -motion-translate-x-in-100 lg:hidden" />
+      <ContainerWithBorder variant="left" className="hidden lg:flex ml-auto px-4 lg:px-5.5 py-4 -motion-translate-x-in-25">
+        <p className="text-muted-foreground">_contacto</p>
+      </ContainerWithBorder>
     </motion.header>
   )
 }
