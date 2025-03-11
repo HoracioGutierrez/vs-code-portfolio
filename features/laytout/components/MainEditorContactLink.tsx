3971111d901d46c39f65ab1d3673b9f658fe320d
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-03-11 16:25:59 -0300

    enhance MainEditorContactLink with motion effects for improved interactivity

--- features/laytout/components/MainEditorContactLink.tsx
+++ features/laytout/components/MainEditorContactLink.tsx
@@ -3,20 +3,30 @@
 import Link from "next/link";
 import ContainerWithBorder from "./ContainerWithBorder";
 import { useMedia } from "react-use";
+import { forwardRef } from "react";
+import { motion } from "motion/react"
 
 function MainEditorContactLink() {
   const isVisible = useMedia("(min-width: 1024px)", false);
 
   if (!isVisible) return null;
 
+  const MotionNextLink = forwardRef<HTMLAnchorElement, React.ComponentPropsWithoutRef<typeof Link>>((props, ref) => {
+    return (
+      <Link {...props} ref={ref} />
+    )
+  })
+
   return (
     <ContainerWithBorder
       variant="bottom-left"
       className="hidden lg:flex ml-auto px-4 lg:px-5.5 py-4"
     >
-      <Link href="/contacto" className="text-muted-foreground">
-        _contacto
-      </Link>
+      <MotionNextLink href="/contacto" className="text-muted-foreground">
+        <motion.span variants={{ tap: { scale: 0.85, color: "#FEA55F" } }}>
+          _contacto
+        </motion.span>
+      </MotionNextLink>
     </ContainerWithBorder>
   );
 }
