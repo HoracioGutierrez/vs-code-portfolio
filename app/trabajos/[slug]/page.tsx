7a62070229c2f727f9de705e606247419d022e88
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-03-31 15:15:56 -0300

    feat(trabajos): add detail page and adjust loading animation delay
    Add a new detail page for the trabajos section and increase the loading animation delay for better user experience

---
+++ app/trabajos/[slug]/page.tsx
@@ -0,0 +1,8 @@
+import * as motion from "motion/react-client"
+
+function TrabajosDetailPage() {
+    return (
+        <motion.div initial="hide" animate="show" exit="exit" className="flex grow p-8 flex-col justify-center items-center">TrabajosDetailPage</motion.div>
+    )
+}
+export default TrabajosDetailPage
