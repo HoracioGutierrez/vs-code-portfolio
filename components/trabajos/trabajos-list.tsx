e743acc5b032a7c34aee82486f1b106a93eb2929
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-03-31 23:26:40 -0300

    refactor(trabajos-list): improve hover animation and layout structure
    Restructured the hover animation and layout in the trabajos-list component to enhance user experience. The changes include smoother transitions, better positioning of elements, and improved visual feedback on hover. This refactor aims to make the component more interactive and visually appealing.

--- components/trabajos/trabajos-list.tsx
+++ components/trabajos/trabajos-list.tsx
@@ -35,21 +35,67 @@ async function TrabajosList({ stack }: TrabajosListProps) {
         <motion.div initial="hide" animate="show" exit="exit" variants={containerVariants} className="w-full h-full grid grid-cols-[repeat(auto-fill,minmax(min(300px,100%),1fr))] gap-5 lg:gap-10 grid-flow-row place-content-start">
             <AnimatePresence>
                 {payload?.map((project, index) => (
-                    <motion.div key={index} variants={itemVariants} whileHover={{ scale: 1.05, transition: { type: "spring", bounce: 0.85, stiffness: 400, damping: 16 } }} className="border border-muted-foreground/50 rounded-md box-border overflow-hidden flex flex-col">
-                        <Image
-                            src={project.thumbnail ? `/images/trabajos/${project.thumbnail}` : "/images/thumbnail.png"}
-                            alt={project.title}
-                            width={300}
-                            height={300}
-                            className="w-full object-cover aspect-video"
-                        />
-                        <div className="p-5 flex flex-col gap-3 grow">
-                            <h3>{project.title}</h3>
-                            <p className="text-muted-foreground text-sm">{project.subtitle}</p>
-                            <Button asChild className="bg-transparent border border-muted-foreground/50 mt-auto hover:bg-accent-2 hover:!text-primary">
-                                <Link href={`/trabajos/${project.slug}`} className="text-sm !text-muted-foreground">Ver proyecto</Link>
-                            </Button>
-                        </div>
+                    <motion.div
+                        key={index}
+                        variants={itemVariants}
+                        //whileHover={{ scale: 1.05, transition: { type: "spring", bounce: 0.85, stiffness: 400, damping: 16 } }}
+                        //whileHover="hover"
+                        className=" flex flex-col relative"
+                    >
+                        <motion.div whileHover="hover" className="h-full" initial="initial">
+                            <motion.div
+                                className="relative aspect-video border border-muted-foreground/50 rounded-md box-border overflow-hidden z-10"
+                            /* variants={{ hover: { aspectRatio: "initial", height: "100%", width: "100%" } }} */
+                            >
+                                <Image
+                                    src={project.thumbnail ? `/images/trabajos/${project.thumbnail}` : "/images/thumbnail.png"}
+                                    alt={project.title}
+                                    fill
+                                    className="w-full object-cover"
+                                />
+                                <motion.div
+                                    className="p-5 flex flex-col gap-3 grow absolute backdrop-blur-lg bg-black/80 z-20 w-full"
+                                    variants={{
+                                        initial: { bottom: -170, opacity: 0 },
+                                        hover: { bottom: 0, opacity: 1 }
+                                    }}
+                                >
+                                    <motion.h3>{project.title}</motion.h3>
+                                    <motion.p
+                                        variants={{
+                                            initial: { opacity: 0, x: 50 },
+                                            hover: { opacity: 1, x: 0, transition: { delay: 0.1 } }
+                                        }}
+                                        className="text-muted-foreground text-sm"
+                                    >{project.subtitle}</motion.p>
+                                    <Button asChild className="bg-transparent border border-muted-foreground/50 mt-auto hover:bg-accent-2 hover:!text-primary">
+                                        <Link href={`/trabajos/${project.slug}`} className="text-sm !text-muted-foreground">Ver proyecto</Link>
+                                    </Button>
+                                </motion.div>
+                            </motion.div>
+                            <motion.div
+                                className="py-2 flex flex-col gap-3 grow"
+                                variants={{
+                                    initial: { y: 0, opacity: 1, transition: { delay: 0.3 } },
+                                    hover: { y: -200, opacity: 0 }
+                                }}
+                            /* variants={{ hover: { position: "absolute", bottom: 0, backgroundColor: "rgba(0,0,0,0.3)", backdropFilter: "blur(1.5)" } }} */
+                            >
+                                <motion.h3
+
+                                >{project.title}</motion.h3>
+                                {/* <motion.p
+                                    variants={{
+                                        
+                                        initial: { opacity: 0 },
+                                        hover: { opacity: 1, transition: { delay: 0.2 } } 
+                                       
+                                    }}
+                                    className="text-muted-foreground text-sm"
+                                >{project.subtitle}</motion.p> */}
+                                {/* */}
+                            </motion.div>
+                        </motion.div>
                     </motion.div>
                 ))}
             </AnimatePresence>
